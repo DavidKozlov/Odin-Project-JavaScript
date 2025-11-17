a3caedb16a772c9a2eb72e2a3eac589f0b6a46b9
DavidKozlov <DavidKozlov24@gmail.com>
2025-11-17 15:44:50 -0800

    GKTGDJ

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const string = "The revolution will not be televised.";
-console.log(string);
+let question = prompt("What's the “official” name of JavaScript?")
+let value = 2
+if (value > 0) {
+  alert(1)
+} else if (value < 0) {
+  alert(-1)
+} else {
+  alert(0)
+}
